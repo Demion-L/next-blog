628f978e391da2159e477726c99de1ef7df4ed57
Demion-L <padovds@ukr.net>
2024-04-12 16:37:48 +0300

    schadcn library added, theme provider added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5'>
       <Link href='/' className='font-bold text-3xl'>
-        Dmytro<span className='text-blue-500'>Blog</span>
+        Dmytro<span className='text-primary'>Blog</span>
       </Link>
     </nav>
   );
